e807e0b48741d372c0808da049ea87926a58500c
Artem Kazaryan <mirony4@yandex.ru>
2023-04-11 23:29:13 +0300

    Решено второе задание (всего 2)

--- Working_with_static_methods.py
+++ Working_with_static_methods.py
@@ -29,9 +29,10 @@ class Fraction:
     def __init__(self, numerator, denominator):
         self.numerator = numerator
         self.denominator = denominator
-        self.float_view = self.numerator / self.denominator
+        self.decimal_view = self.numerator / self.denominator
         Fraction.count_obj += 1
-        print(f'Создана {self.count_obj}-я дробь: {self.numerator}/{self.denominator}. Её десятичное значение равно: {self.float_view}')
+        print(f'Создана {self.count_obj}-я дробь: {self.numerator}/{self.denominator}.'
+              f' Её десятичное значение равно: {self.decimal_view}')
 
     @staticmethod
     def count_ex():
@@ -58,50 +59,74 @@ class Fraction:
         return round(fract1 / fract2, 3)
 
 print()
-fraction1 = Fraction(4, 5)
-fraction2 = Fraction(1, 2)
+fraction1 = Fraction(4, 5).decimal_view
+fraction2 = Fraction(1, 2).decimal_view
 print()
-Fraction.summ(fraction1.float_view, fraction2.float_view)
-Fraction.subtract(fraction1.float_view, fraction2.float_view)
-Fraction.multiplic(fraction1.float_view, fraction2.float_view)
-Fraction.division(fraction1.float_view, fraction2.float_view)
+Fraction.summ(fraction1, fraction2)
+Fraction.subtract(fraction1, fraction2)
+Fraction.multiplic(fraction1, fraction2)
+Fraction.division(fraction1, fraction2)
 print()
 Fraction.count_ex()
+print()
 
-
-
-
-
-
-
-
-
-
-
-# @staticmethod
-# def summ():
-#     Fraction
-
-# def subtraction(self):
-    #
-    # def multiplication(self):
-    #
-    # def division(self):
-
-#
-#
 #
 # Задание 2
 # Создайте класс для конвертирования температуры из
 # Цельсия в Фаренгейт и наоборот. У класса должно быть
-# два статических метода: для перевода из Цельсия в Фаренгейт и для перевода из Фаренгейта в Цельсий. Также
-# класс должен считать количество подсчетов температуры и
+# два статических метода: для перевода из Цельсия в Фаренгейт и для перевода из Фаренгейта в Цельсий.
+# Также класс должен считать количество подсчетов температуры и
 # возвращать это значение с помощью статического метода.
 #
-#
-#
+# Решение:
+
+class TempConv:
+    temp_counts_num = 0
+    def __init__(self, value):
+        self.value = value
+        TempConv.temp_counts_num += 1
+
+
+    @staticmethod
+    def get_temp_counts_num():
+        print(f'Подсчётов температуры произведено: {TempConv.temp_counts_num}')
+
+    @staticmethod
+    def to_Faringate(arg):
+        res = arg * 1.8 + 32
+        print(f'Конвертация: {arg} гр.C => {round(res, 2)} гр.F')
+        return res
+
+    @staticmethod
+    def to_Celsius(arg):
+        res = (arg - 32) / 1.8
+        print(f'Конвертация: {arg} гр.F => {round(res, 2)} гр.C ')
+        return res
+
+print()
+from_Cels_temp1 = TempConv(10)
+from_Cels_temp2 = TempConv(15)
+from_Faring_temp1 = TempConv(41)
+from_Faring_temp2 = TempConv(30)
+
+TempConv.to_Faringate(from_Cels_temp1.value)
+TempConv.to_Faringate(from_Cels_temp2.value)
+TempConv.to_Celsius(from_Faring_temp1.value)
+TempConv.to_Celsius(from_Faring_temp2.value)
+print()
+TempConv.get_temp_counts_num()
+print()
+
+
+
+
+
+
 # Задание 3
 # Создайте класс для перевода из метрической системы
 # в английскую и наоборот. Функциональность необходимо
 # реализовать в виде статических методов. Обязательно
 # реализуйте перевод мер длины.
+
+# Решение:
+
